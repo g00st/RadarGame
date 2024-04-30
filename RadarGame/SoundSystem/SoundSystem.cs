51e0229bf0c1f10f276bf05663ce57f7e7054941
policebunny <136323006+policebunny@users.noreply.github.com>
2024-04-30 10:56:11 +0200

    Loop and NoLoop added for playing freq, Keys I L K M added

--- RadarGame/SoundSystem/SoundSystem.cs
+++ RadarGame/SoundSystem/SoundSystem.cs
@@ -51,54 +51,45 @@ public static class SoundSystem
         context = ALC.CreateContext(device, flags);
     }
 
-    public static  void TrySinusIsUnsafe()
+    public static  void PlaySinusWaveLoop(int orderedSamplefreq, int orderedFreq)
     {
+
         OpenALLoader.LoadLibrary();
         
         // Initialize
-
         ALC.MakeContextCurrent(context);
-
         var version = AL.Get(ALGetString.Version);
         var vendor = AL.Get(ALGetString.Vendor);
         var renderer = AL.Get(ALGetString.Renderer);
         Console.WriteLine(version);
         Console.WriteLine(vendor);
         Console.WriteLine(renderer);
-       
         // Process
         int buffers = 0;   // no need for int* disgusting buffers
-       //  int otherbuffer = AL.GenBuffer(); int othersource = AL.GenBuffer(); // this is LEGAL
+        //  int otherbuffer = AL.GenBuffer(); int othersource = AL.GenBuffer(); // this is LEGAL
         AL.GenBuffers(1, ref buffers);       // no out ?
         AL.GenSources(1, ref source);
         Buffers.Add(buffers);
         Sources.Add(source);
-
         // sampleFreq = 44100;   // example freq is sinus curve speed  c
-        double dt = 2 * Math.PI / sampleFreq;
+        double dt = 2 * Math.PI / orderedSamplefreq;
         double amp = 0.5;
-
         // ------------
         // freq = 440;  // standard freqlänge  lambda
-
-        dataCount = sampleFreq / freq; // f = c / lambda
+        dataCount = orderedSamplefreq / orderedFreq; // f = c / lambda
         sinData = new short[dataCount];
 
         for (int i = 0; i < sinData.Length; ++i)
         {
-            sinData[i] = (short)(amp * short.MaxValue * Math.Sin(i * dt * freq));
+            sinData[i] = (short)(amp * short.MaxValue * Math.Sin(i * dt * orderedFreq));
         }
         
-        AL.BufferData(buffers, ALFormat.Mono16, sinData, sampleFreq); // mag []
-
+        AL.BufferData(buffers, ALFormat.Mono16, sinData, orderedSamplefreq); // mag []
         // AL.BufferData(buffers, ALFormat.Mono16,ref  sinData, sinData.Length, sampleFreq); // ??? short[] nicht zu IntPtr konvertierbar
 
         AL.Source(source, ALSourcei.Buffer, buffers);
         AL.Source(source, ALSourceb.Looping, true);
-
         AL.SourcePlay(source);
-
-
     }
 
     public static void StopPlayingSource()
@@ -126,18 +117,69 @@ public static class SoundSystem
         if(keyboardState.IsKeyDown(Keys.L))
         {
             // play frequenz
-            TrySinusIsUnsafe();
+            PlaySinusWaveLoop(sampleFreq, freq);
+        }
+        if(keyboardState.IsKeyDown(Keys.I))
+        {
+            PlaySinusWaveNoLoop(sampleFreq, freq);
+        }
+        if(keyboardState.IsKeyDown(Keys.B))
+        {
+            sampleFreq = 13655;
+            freq = 494;
+            PlaySinusWaveNoLoop(sampleFreq, freq);
         }
     }
 
     public static void DebugDraw()
     {
         ImGuiNET.ImGui.Begin("Sound");
-        ImGuiNET.ImGui.SliderInt("SampleFrequenz", ref sampleFreq, 40000, 60000);
+        ImGuiNET.ImGui.SliderInt("SampleFrequenz", ref sampleFreq, 4000, 60000);
         ImGuiNET.ImGui.SliderInt("Frequenz", ref freq, 40, 1000);
         ImGuiNET.ImGui.End();
         // ImGuiNET.ImGui.PlotLines("sinData", ref sinData[0], sinData.Length, sinDataIndex, "sinData", 0, 100, new System.Numerics.Vector2(0, 100));
 
+    }
+
+    // sample 13655 freq 494
+    public static void PlaySinusWaveNoLoop(int orderedSampleFreq, int orderedFreq)
+    {
+        OpenALLoader.LoadLibrary();
+        
+        // Initialize
+        ALC.MakeContextCurrent(context);
+        var version = AL.Get(ALGetString.Version);
+        var vendor = AL.Get(ALGetString.Vendor);
+        var renderer = AL.Get(ALGetString.Renderer);
+        Console.WriteLine(version);
+        Console.WriteLine(vendor);
+        Console.WriteLine(renderer);
+        // Process
+        int buffers = 0;   // no need for int* disgusting buffers
+        //  int otherbuffer = AL.GenBuffer(); int othersource = AL.GenBuffer(); // this is LEGAL
+        AL.GenBuffers(1, ref buffers);       // no out ?
+        AL.GenSources(1, ref source);
+        Buffers.Add(buffers);
+        Sources.Add(source);
+        // sampleFreq = 44100;   // example freq is sinus curve speed  c
+        double dt = 2 * Math.PI / orderedSampleFreq;
+        double amp = 0.5;
+        // ------------
+        // freq = 440;  // standard freqlänge  lambda
+        dataCount = orderedSampleFreq / orderedFreq; // f = c / lambda
+        sinData = new short[dataCount];
+
+        for (int i = 0; i < sinData.Length; ++i)
+        {
+            sinData[i] = (short)(amp * short.MaxValue * Math.Sin(i * dt * orderedFreq));
+        }
+
+        AL.BufferData(buffers, ALFormat.Mono16, sinData, orderedSampleFreq); // mag []
+        // AL.BufferData(buffers, ALFormat.Mono16,ref  sinData, sinData.Length, sampleFreq); // ??? short[] nicht zu IntPtr konvertierbar
+
+        AL.Source(source, ALSourcei.Buffer, buffers);
+        AL.Source(source, ALSourceb.Looping, false);
+        AL.SourcePlay(source);
     }
 
     public static void PlayFileDotWave(string filename)
